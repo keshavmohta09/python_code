965ded8fbcb3580e872980dd99d6049681fcc5e6
keshav <keshavmohta09@gmail.com>
2020-09-22 16:58:04 +0530

    reverse.py

--- reverse.py
+++ reverse.py
@@ -1,8 +1,8 @@
 num = int(input("Enter the number: "))
-temp_num = num
+temp = num
 rev = 0
 while num!=0:
-    temp = num%10
+    mod = num%10
     num//=10
-    rev = rev*10+temp
-print("The reverse number of",temp_num,"is",rev)
+    rev = rev*10+mod
+print("The reverse number of %d is %d"%(temp,rev))
